88e636fb1895d82444d2682e13289f057c918a6d
Berk Şengül <berksengul17@gmail.com>
2022-07-21 19:44:39 +0300

    Create basic math functions

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(first, second){
+    return first + second;
+}
+
+function subtract(first, second){
+    return first - second;
+}
+
+function multiply(first, second){
+    return first * second;
+}
+
+function divide(first, second){
+    if(first != 0)
+        return first / second;
+}
+
+function operate(operator, first, second){
+    switch(operator){
+        case "+":
+            add(first, second);
+            break;
+        case "-":
+            subtract(first, second);
+            break;
+        case "*":
+            multiply(first, second);
+            break;
+        case "/":
+            divide(first, second);
+            break;
+    }
+}
